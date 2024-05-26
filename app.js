f27ea82c60a8c60d3b3f8cb1b42838477edf723b
Brieuc DUMORTIER <brieuc.dumortier@viacesi.fr>
2024-05-26 14:12:10 +0200

    Mise à jour des vues

--- app.js
+++ app.js
@@ -1,4 +1,5 @@
 var express = require('express');
+var expressLayouts = require('express-ejs-layouts');
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
@@ -7,6 +8,14 @@ var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
 
 var app = express();
+
+// Configure EJS as the view engine
+app.set('view engine', 'ejs');
+app.set('views', path.join(__dirname, 'views'));
+
+// Use express-ejs-layouts
+app.use(expressLayouts);
+app.set('layout', 'layout'); // Default layout
 
 app.use(logger('dev'));
 app.use(express.json());

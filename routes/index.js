56d0153193fdefc1c0a3bf393ebc0d0f62764f41
kilyion <kilyion.romary@viacesi.fr>
2024-05-27 16:10:07 +0200

    feature(ajout connexion API test)

--- routes/index.js
+++ routes/index.js
@@ -1,8 +1,16 @@
 var express = require('express');
 var router = express.Router();
+var axios = require('axios');
 
-router.get('/', function (req, res, next) {
-  res.render('index', { title: 'Home' });
+router.get('/', function(req, res, next) {
+  axios.get('http://localhost:8000/deliveries') // Assuming your API is running on localhost:3000
+    .then(response => {
+      res.render('index', { title: 'Home', deliveries: response.data });
+    })
+    .catch(error => {
+      console.error('Error fetching deliveries:', error);
+      res.render('index', { title: 'Home', deliveries: [] });
+    });
 });
 
 module.exports = router;
